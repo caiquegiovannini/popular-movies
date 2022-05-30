25c38857c75ecdcf0fc696d341d04ba9f4393556
Caique Giovannini <caique_giovannini@hotmail.com>
2022-05-30 00:18:10 -0300

    Separate renderMovieCard in functions

--- public/scripts.js
+++ public/scripts.js
@@ -49,54 +49,62 @@ function renderMovieCard(movie) {
     movieCard.classList.add('movie');
     moviesList.appendChild(movieCard);
 
-    // add image
-    const movieImageContainer = document.createElement('div');
-    movieImageContainer.classList.add('movie__image');
-    movieCard.appendChild(movieImageContainer);
-    const movieImage = document.createElement('img');
-    movieImage.src = image;
-    movieImage.alt = `${title} image`
-    movieImageContainer.appendChild(movieImage);
+    addMovieImageToCard();
+    addMovieInfoToCard();
+    addMovieDescriptionToCard();
 
-    // movie info
-    const movieInfo = document.createElement('div');
-    movieInfo.classList.add('movie__info');
-    movieCard.appendChild(movieInfo);
+    function addMovieImageToCard() {
+        const movieImageContainer = document.createElement('div');
+        movieImageContainer.classList.add('movie__image');
+        movieCard.appendChild(movieImageContainer);
+        const movieImage = document.createElement('img');
+        movieImage.src = image;
+        movieImage.alt = `${title} image`
+        movieImageContainer.appendChild(movieImage);
+    };
 
-    const movieTitle = document.createElement('h3');
-    movieTitle.classList.add('movie__title');
-    movieTitle.textContent = `${title} (${year})`;
-    movieInfo.appendChild(movieTitle);
+    function addMovieInfoToCard() {
+        const movieInfo = document.createElement('div');
+        movieInfo.classList.add('movie__info');
+        movieCard.appendChild(movieInfo);
+    
+        const movieTitle = document.createElement('h3');
+        movieTitle.classList.add('movie__title');
+        movieTitle.textContent = `${title} (${year})`;
+        movieInfo.appendChild(movieTitle);
+    
+        const movieRateContainer = document.createElement('div');
+        movieRateContainer.classList.add('movie__rate');
+        movieInfo.appendChild(movieRateContainer);
+        const starIcon = document.createElement('img');
+        starIcon.src = 'public/assets/star.svg';
+        starIcon.alt = 'star-icon';
+        movieRateContainer.appendChild(starIcon);
+        const movieRate = document.createElement('p');
+        movieRate.textContent = rating;
+        movieRateContainer.appendChild(movieRate);
+    
+        const favoriteContainer = document.createElement('div');
+        favoriteContainer.classList.add('favorite');
+        movieInfo.appendChild(favoriteContainer);
+        const favoriteIcon = document.createElement('img');
+        favoriteIcon.src = `public/assets/heart${isFavorited ? '-full' : ''}.svg`;
+        favoriteIcon.alt = 'heart-icon';
+        favoriteContainer.appendChild(favoriteIcon);
+        const favoriteText = document.createElement('p');
+        favoriteText.textContent = 'Favoritar';
+        favoriteContainer.appendChild(favoriteText);
+    };
 
-    const movieRateContainer = document.createElement('div');
-    movieRateContainer.classList.add('movie__rate');
-    movieInfo.appendChild(movieRateContainer);
-    const starIcon = document.createElement('img');
-    starIcon.src = 'public/assets/star.svg';
-    starIcon.alt = 'star-icon';
-    movieRateContainer.appendChild(starIcon);
-    const movieRate = document.createElement('p');
-    movieRate.textContent = rating;
-    movieRateContainer.appendChild(movieRate);
-
-    const favoriteContainer = document.createElement('div');
-    favoriteContainer.classList.add('favorite');
-    movieInfo.appendChild(favoriteContainer);
-    const favoriteIcon = document.createElement('img');
-    favoriteIcon.src = `public/assets/heart${isFavorited ? '-full' : ''}.svg`;
-    favoriteIcon.alt = 'heart-icon';
-    favoriteContainer.appendChild(favoriteIcon);
-    const favoriteText = document.createElement('p');
-    favoriteText.textContent = 'Favoritar';
-    favoriteContainer.appendChild(favoriteText);
-
-    //movie description
-    const movieDescriptionContainer = document.createElement('div');
-    movieDescriptionContainer.classList.add('movie__description');
-    movieCard.appendChild(movieDescriptionContainer);
-    const movieDescription = document.createElement('p');
-    movieDescription.textContent = description;
-    movieDescriptionContainer.appendChild(movieDescription);
+    function addMovieDescriptionToCard() {
+        const movieDescriptionContainer = document.createElement('div');
+        movieDescriptionContainer.classList.add('movie__description');
+        movieCard.appendChild(movieDescriptionContainer);
+        const movieDescription = document.createElement('p');
+        movieDescription.textContent = description;
+        movieDescriptionContainer.appendChild(movieDescription);
+    };
 };
+
 
 movies.forEach(movie => renderMovieCard(movie));
